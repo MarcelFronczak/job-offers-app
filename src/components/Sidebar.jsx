fa151d2576ee39dd15e46d28f0fa74a4ca6e372b
Marcel Fronczak <marcel.fronczak@gmail.com>
2022-12-31 15:35:34 +0100

    searchbar event listener added

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Sidebar.css'
 
 function Sidebar() {
+    const [search, setSearch] = useState('');
+
+    function handleChange(e) {
+        setSearch(e.target.value);
+        console.log(search);
+    }
+
   return (
     <sidebar>
       <div className='filters-div'>
-        <input type='text' className='searchbar' placeholder='Search for jobs...'></input>
+            <input
+                value={search}
+                onChange={handleChange}
+                type='text'
+                className='searchbar'
+                placeholder='Search for jobs...'>
+            </input>
         <div className='filters'></div>
       </div>
     </sidebar>
