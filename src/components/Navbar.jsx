0ab1786710add6941c744ce1a3f32e57567fdd5a
Marcel Fronczak <marcel.fronczak@gmail.com>
2023-01-08 19:06:53 +0100

    issue fixed: checkboxes unchecking on event

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,7 +26,7 @@ function Navbar({ setFilter }) {
   }
 
   function handleLevelChange(e) {
-    console.log(e.target.checked)
+    
   }
 
   return (
@@ -71,8 +71,7 @@ function Navbar({ setFilter }) {
                   <p>Experience level</p>
                   {levelDivOpen ? <FontAwesomeIcon icon={faChevronUp} /> : <FontAwesomeIcon icon={faChevronDown} />}
                 </div>
-                {levelDivOpen && (
-                  <div className='levels-list'>
+                <div className={levelDivOpen ? 'list-visible' : 'list-not-visible'}>
                     <label onChange={handleLevelChange} value='internship'>
                       Internship
                       <input type="checkbox" name="" id="internship" value='internship'/>
@@ -94,7 +93,6 @@ function Navbar({ setFilter }) {
                       <input type="checkbox" name="" id="management" />
                     </label>
                   </div>
-                )}
               </fieldset>
 
             </form>
@@ -106,9 +104,10 @@ function Navbar({ setFilter }) {
             <a target='_blank' href="https://www.linkedin.com/in/marcel-fronczak-88523022b/" className='social-icon'><FontAwesomeIcon icon={faLinkedinIn} /></a>
           </div>
         </div>
+    
     </aside>
-    {open && (
-      <aside className='sidebar-mobile'>
+      {/* Mobile sidebar */}
+      <aside className={open ? 'sidebar-mobile' : 'sidebar-mobile-hidden'}>
         <div className="sidebar-content">
           <div className='filters-div'>
             <div className="searchbar-shadow-wrap">
@@ -127,15 +126,30 @@ function Navbar({ setFilter }) {
                   <p>Experience level</p>
                   {levelDivOpen ? <FontAwesomeIcon icon={faChevronUp} /> : <FontAwesomeIcon icon={faChevronDown} />}
                 </div>
-                {levelDivOpen && (
-                  <div className='levels-list'>
-                    <label>Internship<input type="checkbox" name="" id="" /></label>
-                    <label>Entry<input type="checkbox" name="" id="" /></label>
-                    <label>Mid<input type="checkbox" name="" id="" /></label>
-                    <label>Senior<input type="checkbox" name="" id="" /></label>
-                    <label>Management<input type="checkbox" name="" id="" /></label>
+
+                  <div className={levelDivOpen ? 'list-visible' : 'list-not-visible'}>
+                    <label onChange={handleLevelChange} value='internship'>
+                      Internship
+                      <input type="checkbox" name="" id="internship" value='internship'/>
+                    </label>
+                    <label onChange={handleLevelChange} value='entry'>
+                      Entry
+                      <input type="checkbox" name="" id="entry" />
+                    </label>
+                    <label onChange={handleLevelChange} value='mid'>
+                      Mid
+                      <input type="checkbox" name="" id="mid" />
+                    </label>
+                    <label onChange={handleLevelChange} value='senior'>
+                      Senior
+                      <input type="checkbox" name="" id="senior" />
+                    </label>
+                    <label onChange={handleLevelChange} value='management'>
+                      Management
+                      <input type="checkbox" name="" id="management" />
+                    </label>
                   </div>
-                )}
+              
               </fieldset>
 
             </form>
@@ -148,7 +162,6 @@ function Navbar({ setFilter }) {
         </div>
       </aside>
       )
-    }
     </>
   )
 }
