2da89d84ef26403ac4e03ce63450fc94d13e6cdd
Marcel Fronczak <marcel.fronczak@gmail.com>
2023-01-16 20:53:21 +0100

    unused useEffect removed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Navbar from "./components/Navbar";
 import OffersList from './components/OffersList';
