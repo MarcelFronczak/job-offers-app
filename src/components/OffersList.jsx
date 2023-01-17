2a004197f529e1fcd5175183f559bd9a701fe24f
Marcel Fronczak <marcel.fronczak@gmail.com>
2023-01-17 21:57:14 +0100

    pagination added (got to previous/next page)

--- src/components/OffersList.jsx
+++ src/components/OffersList.jsx
@@ -1,15 +1,18 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import Offer from './Offer';
+import Offer from './Offer'
 import './OffersList.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCaretLeft, faCaretRight } from '@fortawesome/free-solid-svg-icons'
 
 function OffersList({ searchbarFilter, level }) {
     const [offers, setOffers] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [page, setPage] = useState(1);
 
     useEffect(() => {
       setLoading(true)
-        axios.get(`https://www.themuse.com/api/public/jobs?${level}page=1`)
+        axios.get(`https://www.themuse.com/api/public/jobs?${level}page=${page}`)
             .then(res => {
                 const data = res.data.results.filter((offer) => {
                 if (offer.name.toLowerCase().includes(searchbarFilter.toLowerCase())) {
@@ -25,7 +28,19 @@ function OffersList({ searchbarFilter, level }) {
             })
             console.log(level);
             
-    }, [searchbarFilter, level])
+    }, [page, searchbarFilter, level])
+
+    const handlePreviousBtnClick = () => {
+      if(page !== 1) {
+        setPage(page => page - 1);
+      }
+    }
+
+    const handleNextBtnClick = () => {
+      if(page !== 99) {
+        setPage(page => page + 1);
+      }
+    }
 
   return (
     <div className='offers-container'>
@@ -47,6 +62,28 @@ function OffersList({ searchbarFilter, level }) {
         loading === false && offers.length === 0 && (
           <div style={{textAlign: 'center', marginTop: '8rem'}}>
               <h2>No jobs found</h2>
+          </div>
+        )
+      }
+      {
+        loading === false && (
+          <div className='page-buttons'>
+            { page > 1 &&
+              (
+                <button onClick={handlePreviousBtnClick} className="page-btn previous-btn">
+                  <FontAwesomeIcon icon={faCaretLeft} className='caret-left'/>
+                  Previous
+                </button>
+              )
+            }
+            { page < 99 &&
+              (
+                <button onClick={handleNextBtnClick} className="page-btn next-btn">
+                  Next
+                  <FontAwesomeIcon icon={faCaretRight} className='caret-right' />
+                </button>
+              )
+            }
           </div>
         )
       }
