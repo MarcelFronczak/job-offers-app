51f0badcc8fd5b8bfcef4fdff9dde05f4ab24a38
Marcel Fronczak <marcel.fronczak@gmail.com>
2023-01-23 23:18:05 +0100

    home sign in button link added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -32,7 +32,7 @@ function Navbar({ setSearchbarFilter, setLevel }) {
               <div className='nav-btns'>
                 <div className='buttons'>
                     <button className='cta btn-sign-up'>Sign Up</button>
-                    <button className='cta btn-sign-in'>Sign In</button>
+                    <Link to='/job-offers-app/signin' style={{textDecoration: 'none'}}><button className='cta btn-sign-in'>Sign In</button></Link>
                 </div>
                 {
                   open ? 
