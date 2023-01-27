5bb388c9df360a740fd962641e4aaac7919125de
Marcel Fronczak <marcel.fronczak@gmail.com>
2023-01-27 13:47:27 +0100

    navigate to homepage after signing in

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -3,13 +3,14 @@ import { useState, useEffect } from 'react'
 import './SignIn.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUser, faXmark } from '@fortawesome/free-solid-svg-icons'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { GoogleButton } from 'react-google-button'
 import { UserAuth } from '../context/AuthContext'
 
 function SignIn() {
   const [loaded, setLoaded] = useState(false);
-  const { googleSignIn } = UserAuth();
+  const { googleSignIn, user } = UserAuth();
+  const navigate = useNavigate();
 
   useEffect(() => {
     setLoaded(true);
@@ -22,6 +23,12 @@ function SignIn() {
       console.log(error)
     }
   }
+
+  useEffect(() => {
+    if(user != null) {
+      navigate('/job-offers-app');
+    }
+  }, [user])
 
   const xMarkStyle = {
     textDecoration: 'none',
@@ -36,14 +43,14 @@ function SignIn() {
     <div className="signin-page">
       <nav className='navbar'>
         <Link to='/job-offers-app' style={{ textDecoration: 'none' }}>
-          <a className='logo' href='#'>
+          <a className='logo' href=''>
               <FontAwesomeIcon icon={faUser} className='logo-icon'/>
               <h1 className='logo-text'>Job<span className='logo-span'>Finder</span></h1>
           </a>
         </Link>
       </nav>
       <div className={loaded ? 'signin-form active' : 'signin-form'}>
-        <Link to='/job-offers-app' style={xMarkStyle}><FontAwesomeIcon icon={faXmark} classname='x'/></Link>
+        <Link to='/job-offers-app' style={xMarkStyle}><FontAwesomeIcon icon={faXmark} className='x'/></Link>
         <h1>Sign In</h1>
         <GoogleButton className='google-btn' onClick={handleGoogleSignIn}/>
       </div>
